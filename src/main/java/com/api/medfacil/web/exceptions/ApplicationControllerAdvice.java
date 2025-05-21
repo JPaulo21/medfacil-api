3b30c0db187101d6cd62feab7d28e5cfa584113e
João Paulo <jp.sport17@gmail.com>
2025-05-20 21:11:26 -0300

    feat: Adicionado tratamento de exception handleInternalServerError e dataIntegrityViolationException

--- src/main/java/com/api/medfacil/web/exceptions/ApplicationControllerAdvice.java
+++ src/main/java/com/api/medfacil/web/exceptions/ApplicationControllerAdvice.java
@@ -3,6 +3,7 @@ package com.api.medfacil.web.exceptions;
 import com.api.medfacil.domain.exceptions.CpfRegisteredException;
 import com.api.medfacil.domain.exceptions.FullNumberRegisteredException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,24 +13,40 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
 
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ResponseError handleInternalServerError(Exception e){
+        return new ResponseError(HttpStatus.INTERNAL_SERVER_ERROR, null, 500, e.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ResponseError dataIntegrityViolationException(DataIntegrityViolationException e){
+        return new ResponseError(HttpStatus.BAD_REQUEST, e.getMessage(), 400, e.getMostSpecificCause().getMessage());
+    }
+
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<ResponseError> authenticationException(AuthenticationException e){
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
-                .body(new ResponseError(HttpStatus.UNAUTHORIZED, 401, e.getMessage()));
+                .body(new ResponseError(HttpStatus.UNAUTHORIZED, null,401, e.getMessage()));
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<ResponseError> BadCredentialsException(BadCredentialsException e){
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
-                .body(new ResponseError(HttpStatus.UNAUTHORIZED, 401, e.getMessage()));
+                .body(new ResponseError(HttpStatus.UNAUTHORIZED, null, 401, e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -38,7 +55,7 @@ public class ApplicationControllerAdvice {
         return ResponseEntity
                 .badRequest()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(new ResponseError(HttpStatus.BAD_REQUEST, 400, result.getFieldError().getDefaultMessage()));
+                .body(new ResponseError(HttpStatus.BAD_REQUEST, result.getFieldError().getField(), 400, result.getFieldError().getDefaultMessage()));
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
@@ -46,7 +63,7 @@ public class ApplicationControllerAdvice {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(new ResponseError(HttpStatus.NOT_FOUND, 404, e.getMessage()));
+                .body(new ResponseError(HttpStatus.NOT_FOUND,null, 404, e.getMessage()));
     }
 
     @ExceptionHandler(CpfRegisteredException.class)
@@ -54,7 +71,7 @@ public class ApplicationControllerAdvice {
         return ResponseEntity
                 .badRequest()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(new ResponseError(HttpStatus.BAD_REQUEST, 400, e.getMessage()));
+                .body(new ResponseError(HttpStatus.BAD_REQUEST,null, 400, e.getMessage()));
     }
 
     @ExceptionHandler(FullNumberRegisteredException.class)
@@ -62,7 +79,7 @@ public class ApplicationControllerAdvice {
         return ResponseEntity
                 .badRequest()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(new ResponseError(HttpStatus.BAD_REQUEST, 400, e.getMessage()));
+                .body(new ResponseError(HttpStatus.BAD_REQUEST,null, 400, e.getMessage()));
     }
 
 
