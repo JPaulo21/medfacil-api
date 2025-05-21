47d29cb6e0e988d739a016fce70a5ab9eb13196c
João Paulo <jp.sport17@gmail.com>
2025-05-20 21:19:51 -0300

    feat: Adicionada url da aplicação ao subir completamente

--- src/main/java/com/api/medfacil/MedfacilApplication.java
+++ src/main/java/com/api/medfacil/MedfacilApplication.java
@@ -1,13 +1,39 @@
 package com.api.medfacil;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class MedfacilApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MedfacilApplication.class, args);
+	private static final Logger log = LoggerFactory.getLogger(MedfacilApplication.class);
+
+	public static void main(String[] args) throws UnknownHostException {
+		SpringApplication app = new SpringApplication(MedfacilApplication.class);
+		Environment env = app.run(args).getEnvironment();
+
+		String info = """
+				\n-----------------------------------------------------------------------------------------------------------------------------------------------
+				 Application {} is running!
+				-----------------------------------------------------------------------------------------------------------------------------------------------
+				Access URLs:
+					Local:\t\thttp://localhost:{}/medfacil
+					External:\thttp://{}:{}/medfacil/
+					Docs:\t\thttp://{}:{}/medfacil/swagger-ui/index.html
+				-----------------------------------------------------------------------------------------------------------------------------------------------
+				""";
+		log.info(info, env.getProperty("spring.application.name"),
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"));
 	}
 
 }
